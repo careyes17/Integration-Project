c9c067968aabf13d8ec48ae1793e4ddf0f136b6b
careyes4820 <careyes4820@L438R5S7.STUDENT.ad.fgcu.edu>
2018-02-13 19:05:17 -0500

    Added Name to all files

--- Practice/src/User.java
+++ Practice/src/User.java
@@ -1,24 +1,24 @@
+//Carlos Reyes
 
 public class User {
-//These will be used later in development	
-private String difficultySelection = new String();
-private int age;
+	// These will be used later in development
+	private String difficultySelection = new String();
+	private int age;
 
-public String getDifficultySelection()
-{
-    return this.difficultySelection;
-}
-public void setDifficultySelection(String difficultySelection)
-{
-     this.difficultySelection = difficultySelection;
-}
-public int getAge()
-{
-    return this.age;
-}
-public void setAge(int age)
-{
-     this.age = age;
-}
+	public String getDifficultySelection() {
+		return this.difficultySelection;
+	}
+
+	public void setDifficultySelection(String difficultySelection) {
+		this.difficultySelection = difficultySelection;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 
 }
